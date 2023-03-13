57cf9484f0b853e79005989c5f2e2f29dbac3756
Arihant Agnihotri <arihantagnihotri2106@gmail.com>
2023-03-14 01:34:02 +0530

    Add components

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
-import logo from './logo.svg';
-import './App.css';
+import { GoogleOAuthProvider } from '@react-oauth/google';
+import Messenger from './components/Messenger';
+
 
 function App() {
+  const client_id = '662808130025-iofkcrpmrt29em6btafujrjtk9f0gpg1.apps.googleusercontent.com'
   return (
-    <div className="App">
-     Hello 
-    </div>
+    <GoogleOAuthProvider clientId={client_id}><Messenger /></GoogleOAuthProvider>
   );
 }
 
